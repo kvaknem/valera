9971030483f5ecf10cd0a7fb6645f02c8c00f3a2
Sergei Kulev <skulev@newagesol.co>
2022-06-24 18:04:31 +0300

    fix: tttttrrtrrt

---
+++ lesson1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.8
+import requests
+import pdb
+
+
+print("ttt123456")
+
+print("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu")
+
+
+print("uuu+++++++++++++++++++++77777777777777777")
+
+
+r = requests.get('https://www.cloudflare.com/ips-v4')
+
+pdb.set_trace()
